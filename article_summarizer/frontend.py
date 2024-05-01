9b55c4d9ecc9bc90388d364bd78e466baf80a1d5
Karn Wong <karn@karnwong.me>
2024-05-01 20:06:04 +0700

    move text input to sidebar

--- article_summarizer/frontend.py
+++ article_summarizer/frontend.py
@@ -5,8 +5,9 @@ from article_summarizer.summarizer import summarize_article
 
 st.title("Article Summarizer")
 
-url = st.text_input(label="Enter article url", key="url")
-submit_button = st.button("Submit", key="submit")
+with st.sidebar:
+    url = st.text_input(label="Enter article url", key="url")
+    submit_button = st.button("Submit", key="submit")
 
 if url and submit_button:
     title, text = extract_article(url)

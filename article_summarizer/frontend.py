1d6719cdb96f97298500216dc460285fd89e4f69
Karn Wong <karn@karnwong.me>
2024-05-16 17:10:08 +0700

    update model

--- article_summarizer/frontend.py
+++ article_summarizer/frontend.py
@@ -18,7 +18,9 @@ st.title("Article Summarizer")
 with st.sidebar:
     url = st.text_input(label="Enter article url", key="url")
     submit_button = st.button("Submit", key="submit")
-    model_option = st.radio("Select model", ("gemma:7b", "kahnwong/typhoon-1.5:8b"))
+    model_option = st.radio(
+        "Select model", ("kahnwong/gemma-1.1-7b", "kahnwong/typhoon-1.5:8b")
+    )
 
 if url and submit_button:
     title, text = extract_article(url)

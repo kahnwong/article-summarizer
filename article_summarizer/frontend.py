9fece50c8f7bd9b7c85c41755b209849ee395752
Karn Wong <karn@karnwong.me>
2024-05-16 14:13:32 +0700

    add radio button for model options

--- article_summarizer/frontend.py
+++ article_summarizer/frontend.py
@@ -18,16 +18,10 @@ st.title("Article Summarizer")
 with st.sidebar:
     url = st.text_input(label="Enter article url", key="url")
     submit_button = st.button("Submit", key="submit")
+    model_option = st.radio("Select model", ("gemma:7b", "kahnwong/typhoon-1.5:8b"))
 
 if url and submit_button:
     title, text = extract_article(url)
     st.header(title)
 
-    get_summary(text, model_name="gemma:7b")
-
-    # summary = summarize_article(text)
-    #
-    # st.header(title)
-    # st.markdown("---")
-    #
-    # st.write_stream(summary)
+    get_summary(text, model_name=model_option)

c2fee38d5654e57c8fa7d5677652b8b8cffe6ab9
Karn Wong <karn@karnwong.me>
2024-07-01 15:56:16 +0700

    extract getUrl method

--- cmd/root.go
+++ cmd/root.go
@@ -87,24 +87,8 @@ var rootCmd = &cobra.Command{
 		// Clears the screen
 		ClearScreen()
 
-		// validate input
-		var url string
-		if len(args) == 0 {
-			urlFromClipboard := ReadFromClipboard()
-			if urlFromClipboard != "" {
-				if strings.HasPrefix(urlFromClipboard, "https://") {
-					url = urlFromClipboard
-				}
-			}
-		}
-		if url == "" {
-			if len(args) == 0 {
-				fmt.Println("Please specify URL")
-				os.Exit(1)
-			} else if len(args) == 1 {
-				url = args[0]
-			}
-		}
+		// get url
+		url := getUrl(args)
 
 		// extract article
 		article, err := extractArticle(url)
--- cmd/utils.go
+++ cmd/utils.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"runtime"
+	"strings"
 
 	"golang.design/x/clipboard"
 )
@@ -39,4 +40,27 @@ func ClearScreen() {
 	if err != nil {
 		fmt.Println(err)
 	}
+}
+
+func getUrl(args []string) string {
+	var url string
+
+	if len(args) == 0 {
+		urlFromClipboard := ReadFromClipboard()
+		if urlFromClipboard != "" {
+			if strings.HasPrefix(urlFromClipboard, "https://") {
+				url = urlFromClipboard
+			}
+		}
+	}
+	if url == "" {
+		if len(args) == 0 {
+			fmt.Println("Please specify URL")
+			os.Exit(1)
+		} else if len(args) == 1 {
+			url = args[0]
+		}
+	}
+
+	return url
 }

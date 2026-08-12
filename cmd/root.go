3a1fdeeee293568f1650a0ddee966d553b748576
Karn Wong <karn@karnwong.me>
2026-08-12 11:47:27 +0700

    feat: return to first page when mark entry as read

--- cmd/root.go
+++ cmd/root.go
@@ -38,72 +38,77 @@ var rootCmd = &cobra.Command{
 	Use:   "article-summarizer",
 	Short: "Summarize an article with LLM",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Clears the screen
-		if err := core.ClearScreen(); err != nil {
-			slog.Error("Failed to clear screen", "error", err)
-			os.Exit(1)
-		}
-
-		// ------------ get entries ------------ //
-		entries, err := wallabagClient.GetEntries()
-		if err != nil {
-			slog.Error("Cannot obtain articles from Wallabag")
-			os.Exit(1)
-		}
-
-		// ------------ select article ------------ //
-		formEntries := huh.NewForm(
-			huh.NewGroup(
-				huh.NewSelect[string]().
-					Title("Choose an article to summarize").
-					Options(
-						createFormOptions(entries)...,
-					).
-					Value(&entryTitle),
-			),
-		)
-		err = formEntries.Run()
-		if err != nil {
-			slog.Error("Form error", "error", err)
-			os.Exit(1)
-		}
-
-		// ------------ summarize ------------ //
-		fmt.Printf("========== %s ==========\n", entryTitle)
-
-		var selectedEntry wallabago.Item
-		for _, entry := range entries {
-			if entry.Title == entryTitle {
-				selectedEntry = entry
+		for {
+			// Clears the screen
+			if err := core.ClearScreen(); err != nil {
+				slog.Error("Failed to clear screen", "error", err)
+				os.Exit(1)
 			}
-		}
 
-		if _, err := core.SummarizeArticle(selectedEntry, "cli"); err != nil {
-			slog.Error("Failed to summarize article", "error", err)
-			os.Exit(1)
-		}
+			// ------------ get entries ------------ //
+			entries, err := wallabagClient.GetEntries()
+			if err != nil {
+				slog.Error("Cannot obtain articles from Wallabag")
+				os.Exit(1)
+			}
 
-		// ------------ mark as read ------------ //
-		formMarkAsRead := huh.NewForm(
-			huh.NewGroup(
-				huh.NewConfirm().
-					Title("Mark article as read?").
-					Value(&markAsRead).
-					Affirmative("Yes").
-					Negative("No"),
-			),
-		)
-		if err := formMarkAsRead.Run(); err != nil {
-			slog.Error("Form error", "error", err)
-			os.Exit(1)
-		}
+			// ------------ select article ------------ //
+			entryTitle = ""
+			formEntries := huh.NewForm(
+				huh.NewGroup(
+					huh.NewSelect[string]().
+						Title("Choose an article to summarize").
+						Options(
+							createFormOptions(entries)...,
+						).
+						Value(&entryTitle),
+				),
+			)
+			err = formEntries.Run()
+			if err != nil {
+				slog.Error("Form error", "error", err)
+				os.Exit(1)
+			}
 
-		if markAsRead {
+			// ------------ summarize ------------ //
+			fmt.Printf("========== %s ==========\n", entryTitle)
+
+			var selectedEntry wallabago.Item
+			for _, entry := range entries {
+				if entry.Title == entryTitle {
+					selectedEntry = entry
+				}
+			}
+
+			if _, err := core.SummarizeArticle(selectedEntry, "cli"); err != nil {
+				slog.Error("Failed to summarize article", "error", err)
+				os.Exit(1)
+			}
+
+			// ------------ mark as read ------------ //
+			markAsRead = false
+			formMarkAsRead := huh.NewForm(
+				huh.NewGroup(
+					huh.NewConfirm().
+						Title("Mark article as read?").
+						Value(&markAsRead).
+						Affirmative("Yes").
+						Negative("No"),
+				),
+			)
+			if err := formMarkAsRead.Run(); err != nil {
+				slog.Error("Form error", "error", err)
+				os.Exit(1)
+			}
+
+			if !markAsRead {
+				return
+			}
+
 			if err := wallabagClient.MarkEntryAsRead(selectedEntry.ID); err != nil {
 				slog.Error("Failed to mark entry as read", "error", err)
 				os.Exit(1)
 			}
-			fmt.Println("Marked article as read")
 		}
 	},
 }
